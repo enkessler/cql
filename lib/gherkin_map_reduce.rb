ad67048dc65f61eaf7bcd0ce5a35131c19b0fee0
jdfolino <jdfolino@gmail.com>
2012-07-31 12:16:08 -0700

    Used core ruby array functions

--- lib/gherkin_map_reduce.rb
+++ lib/gherkin_map_reduce.rb
@@ -2,31 +2,19 @@ module GQL
   class MapReduce
 
     def self.overview input
-      input.map { |a| a['name'] }
+      input.map{|a| a['name'] }
     end
 
     def self.uri input
-      input.map { |a| a['uri'] }
+      input.map{|a| a['uri'] }
     end
 
     def self.find_feature input, feature_to_find
-      feature_found = nil
-      input.each do |feature|
-        if feature['name'] == feature_to_find
-          feature_found = feature
-        end
-      end
-      feature_found
+      input.find{|feature| feature['name'] == feature_to_find }
     end
 
     def self.filter_features_by_tag input, *tags_to_find
-      features = []
-      input.each do |feature|
-          if (feature['tags'] != nil  and has_tags(feature['tags'], tags_to_find.flatten))
-            features.push feature
-          end
-      end
-      features
+      input.find_all{|feature| has_tags feature['tags'], tags_to_find.flatten }
     end
 
     def self.scenario_by_feature_and_tag input, feature_to_find, condition, *tags_to_find
@@ -34,7 +22,7 @@ module GQL
       input.each do |feature|
         if feature['name'] == feature_to_find
           feature['elements'].each do |element|
-            if (element['name'] != "") and element['type'] == "scenario" and has_tags(element['tags'], tags_to_find) == condition
+            if element['type'] == "scenario" and has_tags(element['tags'], tags_to_find) == condition
               scenarios.push element['name']
             end
           end
@@ -51,39 +39,30 @@ module GQL
     end
 
     def self.all input, args
-      scenarios = []
+      results = []
       input = [find_feature(input, args['feature'])] if args.has_key?('feature')
       input.each do |feature|
         feature['elements'].each do |element|
-          scenarios.push element['name'] if (element['name'] != "") and element['type'] == args['what']
+          results.push element['name'] if element['type'] == args['what']
         end
       end
-      scenarios
+      results
     end
 
     def self.tags input
       tags = Set.new
       input.each do |feature|
         feature['elements'].each do |element|
-          if element['tags'] != nil
-            element['tags'].each do |tag|
-              tags.add tag['name']
-            end
-          end
+          break if element['tags'] == nil
+          element['tags'].each { |tag| tags.add tag['name'] }
         end
       end
       tags.to_a
     end
 
-    def self.has_tags tags_given, tags_for_search
-      return false if tags_given == nil
-      tags_given = tags_given.map { |t| t["name"] }
-      found = 0
-      tags_for_search.each do |tag_for_search|
-        found = found + 1 if tags_given.include?(tag_for_search)
-      end
-      found == tags_for_search.size
+    def self.has_tags given, search
+      return false if given == nil
+      search.count{|tag_for_search| given.map{|t| t["name"]}.include?(tag_for_search)}==search.size
     end
-
   end
 end
