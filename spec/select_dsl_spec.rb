03ee724bd0ff4040137897a3d54e4af73dc6a430
jdfolino <jdfolino@gmail.com>
2012-07-30 22:10:09 -0700

    separated specs for the dsl

--- spec/select_dsl_spec.rb
+++ spec/select_dsl_spec.rb
@@ -2,41 +2,6 @@ require 'rspec'
 require File.dirname(__FILE__) + "/../lib/" + "gherkin_repo"
 
 describe "cql" do
-
-  describe 'filter' do
-    it 'should filter by a single tag' do
-      gs = GQL::GherkinRepository.new File.dirname(__FILE__) + "/../fixtures/features/tagged_features"
-
-      result = gs.query do
-        select names
-        from features
-        with tag '@one'
-      end
-
-      result.should == ["Test Feature", "Test3 Feature"]
-
-      result = gs.query do
-        select names
-        from features
-        with tag '@two'
-      end
-
-      result.should == ["Test2 Feature", "Test3 Feature"]
-    end
-
-    it 'should filter by a multiple tags' do
-      gs = GQL::GherkinRepository.new File.dirname(__FILE__) + "/../fixtures/features/tagged_features"
-
-      result = gs.query do
-        select names
-        from features
-        with tags '@one', '@two'
-      end
-      result.should == ["Test3 Feature"]
-
-    end
-  end
-
   describe "select" do
     it 'should find the feature file names' do
       gs = GQL::GherkinRepository.new File.dirname(__FILE__) + "/../fixtures/features/simple"
