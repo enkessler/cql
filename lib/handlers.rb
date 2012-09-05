4df21ccfb904a0729d2b6441df801eb2395e6cd4
jdfolino <jdfolino@gmail.com>
2012-09-04 19:13:53 -0700

    refactoring

--- lib/handlers.rb
+++ lib/handlers.rb
@@ -1,37 +1,37 @@
 module CQL
   class SsoHandlerChain
-    def has_tags given, search
-      return false if given == nil
-      search.count { |tag_for_search| given.map { |t| t["name"] }.include?(tag_for_search) }==search.size
+    def has_tags given_tags, search
+      return false if given_tags == nil
+      search.count { |tag_for_search| given_tags.map { |given_tag| given_tag["name"] }.include?(tag_for_search) }==search.size
+    end
+
+    def comparison_operator(fn)
+      operator = fn.split("_")[1]
+      {"lt"=>'<', 'lte'=>'<=', 'gt'=>'>', 'gte'=>'>='}[operator]
     end
 
     def tc_handler(input, fn, amount)
-      what, operator = fn.split "_"
-      operator_map = {"lt"=>'<', 'lte'=>'<=', 'gt'=>'>', 'gte'=>'>='}
       input.each_with_index do |feature, index|
         filtered_elements= feature['elements'].find_all do |sso|
-          sso['tags'].size.send(operator_map[operator], amount)
+          sso['tags'].size.send(comparison_operator(fn), amount)
         end
         input[index]['elements'] = filtered_elements
       end
     end
 
     def lc_handle(args, input, fn)
-      what, operator = fn.split "_"
-      operator_map = {"lt"=>'<', 'lte'=>'<=', 'gt'=>'>', 'gte'=>'>='}
       input.each_with_index do |feature, index|
         filtered_elements= feature['elements'].find_all do |sso|
-          sso['steps'].size.send(operator_map[operator], args[fn])
+          sso['steps'].size.send(comparison_operator(fn), args[fn])
         end
         input[index]['elements'] = filtered_elements
       end
     end
 
-    def line_handler(args, input)
+    def line_handler(line_to_match, input)
       input.each_with_index do |feature, index|
         filtered_elements= feature['elements'].find_all do |sso|
           raw_step_lines = sso['steps'].map { |sl| sl['name'] }
-          line_to_match = args['line'].first
           result = nil
           if line_to_match.class == String
             result = raw_step_lines.include? line_to_match
@@ -51,10 +51,10 @@ module CQL
 
     def tag_search_handler(args, input)
       input.each_with_index do |feature, index|
-        filtered_elements= feature['elements'].find_all do |sso|
+        features_with_contents_filtered = feature['elements'].find_all do |sso|
           has_tags(sso['tags'], args['tags'])
         end
-        input[index]['elements'] = filtered_elements
+        input[index]['elements'] = features_with_contents_filtered
       end
     end
 
@@ -65,7 +65,7 @@ module CQL
       %w(lc_lt lc_lte lc_gt lc_gte).each do |fn|
         lc_handle(args, input, fn) if args.has_key?(fn)
       end
-      line_handler(args, input) if args.has_key?('line')
+      line_handler(args['line'].first, input) if args.has_key?('line')
       tag_search_handler(args, input) if args.has_key?('tags')
       input
     end
