5cfb0dbc8427dd44319262308b5fa6505cf60e84
jdfolino <jdfolino@gmail.com>
2012-09-08 15:06:55 -0700

    more and more refactoring

--- lib/repo.rb
+++ lib/repo.rb
@@ -4,6 +4,26 @@ require 'stringio'
 require File.dirname(__FILE__) + "/dsl"
 
 module CQL
+
+  class Query
+    include Dsl
+    attr_reader :data, :what
+
+    def initialize features, &block
+      @data = features
+      @data = self.instance_eval(&block)
+
+      @data= CQL::MapReduce.filter_sso(@data, 'what'=>@from[0, @from.size-1]) if @from != "features"
+      result = Array.new(@data.size)
+      result = result.map { |e| {} }
+      @what.each do |w|
+        CQL::MapReduce.send(w, @data).each_with_index { |e, i| result[i][w]=e }
+      end
+      @data = result.size == 1 ? result.first : result
+    end
+  end
+
+
   class Repository
     include Dsl
     attr_reader :parsed_feature_files
