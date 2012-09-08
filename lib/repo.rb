6f599638a89e3b1b8dd6fd24b74f58b4daed00fa
jdfolino <jdfolino@gmail.com>
2012-09-08 15:13:33 -0700

    more and more refactoring

--- lib/repo.rb
+++ lib/repo.rb
@@ -9,17 +9,21 @@ module CQL
     include Dsl
     attr_reader :data, :what
 
+    def format_to_ary_of_hsh data
+      result = Array.new(data.size)
+      result = result.map { |e| {} }
+      @what.each do |w|
+        CQL::MapReduce.send(w, data).each_with_index { |e, i| result[i][w]=e }
+      end
+      result.size == 1 ? result.first : result
+    end
+
     def initialize features, &block
       @data = features
       @data = self.instance_eval(&block)
 
       @data= CQL::MapReduce.filter_sso(@data, 'what'=>@from[0, @from.size-1]) if @from != "features"
-      result = Array.new(@data.size)
-      result = result.map { |e| {} }
-      @what.each do |w|
-        CQL::MapReduce.send(w, @data).each_with_index { |e, i| result[i][w]=e }
-      end
-      @data = result.size == 1 ? result.first : result
+      @data= format_to_ary_of_hsh(@data)
     end
   end
 
