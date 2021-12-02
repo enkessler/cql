a96a3975a25f6c57929521073b58f24ab5f283f0
Eric Kessler <morrow748@gmail.com>
2021-12-02 00:21:33 -0500

    Switch to GitHub Actions
    Now using GitHub Actions instead of using Travis CI and AppVeyor.

--- .simplecov
+++ .simplecov
@@ -1,4 +1,16 @@
+require 'simplecov-lcov'
+
 SimpleCov.command_name(ENV['CQL_SIMPLECOV_COMMAND_NAME'])
+
+# Coveralls GitHub Action needs an lcov formatted file
+SimpleCov::Formatter::LcovFormatter.config do |config|
+  config.report_with_single_file = true
+  config.lcov_file_name = 'lcov.info'
+end
+
+# Also making a more friendly HTML file
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter,
+                                                                SimpleCov::Formatter::LcovFormatter])
 
 SimpleCov.start do
   root __dir__
--- rakefiles/reporting_tasks.rb
+++ rakefiles/reporting_tasks.rb
@@ -1,13 +1,5 @@
 namespace 'cql' do
 
-  desc 'Removes the current code coverage data'
-  task :clear_code_coverage do
-    puts Rainbow('Clearing coverage directory...').cyan
-    code_coverage_directory = "#{__dir__}/../coverage"
-
-    FileUtils.remove_dir(code_coverage_directory, true)
-  end
-
   desc 'Removes the contents of the test reporting directory'
   task :clear_report_directory do
     puts Rainbow('Clearing report directory...').cyan
@@ -17,6 +9,6 @@ namespace 'cql' do
   end
 
   desc 'Removes existing test results and code coverage'
-  task :clear_old_results => %i[clear_report_directory clear_code_coverage] # rubocop:disable Style/HashSyntax
+  task :clear_old_results => %i[clear_report_directory] # rubocop:disable Style/HashSyntax
 
 end
